ba3899f3624bb78ca859141f710bd182f14dcf77
bha4 <saravanan.bharathi1994@gmail.com>
2024-05-09 16:05:32 +0530

    2nd Commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from 'react'
+import ExpenseItem from './components/ExcepenseItem';
 function App() {
   return (
     <div className="App">
+      <ExpenseItem/>
       <h1>Hello React</h1>
     </div>
   );
 }
+
 
 export default App;
