415ec623b5b5426c0d64c34c3890a0553b0a48c7
bha4 <saravanan.bharathi1994@gmail.com>
2024-05-10 11:03:49 +0530

    8th commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -6,7 +6,7 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <ExpenseDate  date={props.date}/>
+      <ExpensesDate  date={props.date}/>
       <div className="expense-item__location">{props.location}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
