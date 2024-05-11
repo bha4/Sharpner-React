2cceab92e73f65f7163af66d004e04311aeafb4f
bha4 <saravanan.bharathi1994@gmail.com>
2024-05-11 11:01:59 +0530

    9th commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,17 +4,20 @@ import Card from "./Card";
 
 
 function ExpenseItem(props) {
-
+  function buttonClickHandler(event){
+    console.log("Button clicked")
+  }
   return (
     <Card className="expense-item">
-      <ExpensesDate  date={props.date}/>
-      <div className="expense-item__location">{props.location}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
-      </div>
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.price}</div>
+        </div>
+        <button onClick={buttonClickHandler}>Change Title</button>
     </Card>
   );
 }
+
 
 export default ExpenseItem;
